8e017defd89329484e98719ae73403a26773efa3
junhakjang <100299253+junhakjang@users.noreply.github.com>
2022-09-19 14:34:45 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,15 +2,15 @@ input.onButtonPressed(Button.A, function () {
     NUmber += 1
     basic.showString("" + (NUmber))
 })
+input.onButtonPressed(Button.AB, function () {
+    radio.sendNumber(NUmber)
+})
 input.onButtonPressed(Button.B, function () {
     NUmber += -1
     basic.showString("" + (NUmber))
 })
-input.onGesture(Gesture.Shake, function () {
-    radio.sendString("" + (NUmber))
-})
 let NUmber = 0
-radio.setGroup(69)
+radio.setGroup(23)
 NUmber = 0
 basic.forever(function () {
 	
